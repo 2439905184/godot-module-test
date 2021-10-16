30913ac1b37e7e6e0a0ed4d085da39c60076ca71
2439905184 <suchenglong018@126.com>
2021-10-16 14:43:15 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -10,9 +10,9 @@ int Hello::get_total() const
 }
 void Hello::_bind_methods()
 {
-    ClassDB::bind_method(D_METHOD("add", "value"), &Summator::add);
-    ClassDB::bind_method(D_METHOD("reset"), &Summator::reset);
-    ClassDB::bind_method(D_METHOD("get_total"), &Summator::get_total);
+    ClassDB::bind_method(D_METHOD("add", "value"), &Hello::add);
+    ClassDB::bind_method(D_METHOD("reset"), &Hello::reset);
+    ClassDB::bind_method(D_METHOD("get_total"), &Hello::get_total);
 }
 Hello::Hello()
 {
